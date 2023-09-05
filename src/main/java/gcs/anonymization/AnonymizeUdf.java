6029c18d1c96e6428597e304713d0b16dd167367
Inryatt <inrymail@gmail.com>
2023-09-05 18:31:40 +0100

    Fixed 'wrong' tab escaping for ksql to work correctly

--- src/main/java/gcs/anonymization/AnonymizeUdf.java
+++ src/main/java/gcs/anonymization/AnonymizeUdf.java
@@ -8,28 +8,26 @@ import io.confluent.ksql.function.udf.UdfParameter;
 
 import java.lang.String;
 
-@UdfDescription(name = "anonymize",
+@UdfDescription(name = "anonymize_nginx",
         author = "SARAI",
         version = "0.0.1",
-        description = "Functions for SARAI data anonimization.")
+        description = "Functions for SARAI data anonymization.")
 public class AnonymizeUdf {
 
-    @Udf(description = "Anonimize data coming from the Firewall.")
-    public String anonymize(@UdfParameter String inputUnfiltered) {
+    @Udf(description = "Anonymize data coming from the Firewall.")
+    public String anonymizeNginx(@UdfParameter String inputUnfiltered) {
         // String aux = Filters.removeIPs(inputUnfiltered);
         // aux = Filters.removeIDs(aux);
-        String aux = Filters.removeUserNames_v2(inputUnfiltered,"usrName");
+        // String aux = Filters.removeUserNames_v2(inputUnfiltered,"usrName");
+        String aux = Filters.removeFieldContents(inputUnfiltered,"cookie");
         // Uncomment for final deployment
         // aux = Filters.removeUserNames_v2(inputUnfiltered,"src_user_name");
                     // aux = Filters.removeUserNames_v2(inputUnfiltered,"src_user_dn");
                     // aux = Filters.removeUserNames_v2(inputUnfiltered,"originsicname");
                     // aux = Filters.removeUserNames_v2(inputUnfiltered,"src_user_dn");
 
-        return aux;
+         return aux;
     }
 
-    @Udf(description = "TODO")
-    public String anonymize(@UdfParameter String filebeatType, @UdfParameter String inputUnfiltered) {
-        return "";
-    }
+
 }
